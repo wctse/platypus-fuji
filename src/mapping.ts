aefe3f5e1befe3c8de457f05aa2862c58a4f9120
wctse <wchak.tse@gmail.com>
2021-11-16 12:06:42 +0800

    updated events

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,102 +1,71 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import {
   Platypus,
-  Upgraded,
-  AssetAdded,
   Deposit,
-  DevUpdated,
-  OracleUpdated,
-  OwnershipTransferred,
-  Paused,
-  PriceDeviationUpdated,
   Swap,
-  Unpaused,
   Withdraw
 } from "../generated/Platypus/Platypus"
-import { ExampleEntity } from "../generated/schema"
+import {
+  Deposit,
+  Swap,
+  Withdraw
+} from "../generated/schema"
 
-export function handleUpgraded(event: Upgraded): void {
+export function handleDeposit(event: Deposit): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let depositID = event.transaction.hash.toHexString().concat("-").concat(event.logIndex.toString())
+  let entity = Deposit.load(depositID);
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
   if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    entity = new Deposit(depositID);
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  entity.sender = event.params.sender;
+  entity.token = event.params.token;
+  entity.amount = event.params.amount;
+  entity.liquidity = event.params.liquidity;
+  entity.to = event.params.to;
+  entity.timestamp = event.block.timestamp;
 
-  // Entity fields can be set based on event parameters
-  entity.implementation = event.params.implementation
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.admin(...)
-  // - contract.implementation(...)
-  // - contract.assetOf(...)
-  // - contract.deposit(...)
-  // - contract.getC1(...)
-  // - contract.getDev(...)
-  // - contract.getHaircutRate(...)
-  // - contract.getMaxPriceDeviation(...)
-  // - contract.getPriceOracle(...)
-  // - contract.getRetentionRatio(...)
-  // - contract.getSlippageParamK(...)
-  // - contract.getSlippageParamN(...)
-  // - contract.getTokenAddresses(...)
-  // - contract.getWETH(...)
-  // - contract.getWETHForwarder(...)
-  // - contract.getXThreshold(...)
-  // - contract.owner(...)
-  // - contract.paused(...)
-  // - contract.quotePotentialSwap(...)
-  // - contract.quotePotentialWithdraw(...)
-  // - contract.quotePotentialWithdrawFromOtherAsset(...)
-  // - contract.swap(...)
-  // - contract.withdraw(...)
-  // - contract.withdrawFromOtherAsset(...)
+  entity.save();
 }
 
-export function handleAssetAdded(event: AssetAdded): void {}
 
-export function handleDeposit(event: Deposit): void {}
+export function handleSwap(event: Swap): void {
+  let swapID = event.transaction.hash.toHexString().concat("-").concat(event.logIndex.toString())
+  let entity = Swap.load(swapID);
 
-export function handleDevUpdated(event: DevUpdated): void {}
+  if (!entity) {
+    entity = new Swap(swapID);
+  }
 
-export function handleOracleUpdated(event: OracleUpdated): void {}
+  entity.sender = event.params.sender;
+  entity.fromToken = event.params.fromToken;
+  entity.toToken = event.params.toToken;
+  entity.fromAmount = event.params.fromAmount;
+  entity.toAmount = event.params.toAmount;
+  entity.to = event.params.to;
+  entity.timestamp = event.block.timestamp;
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+  entity.save();
+}
 
-export function handlePaused(event: Paused): void {}
 
-export function handlePriceDeviationUpdated(
-  event: PriceDeviationUpdated
-): void {}
+export function handleWithdraw(event: Withdraw): void {
+  let withdrawID = event.transaction.hash.toHexString().concat("-").concat(event.logIndex.toString())
+  let entity = Swap.load(withdrawID);
 
-export function handleSwap(event: Swap): void {}
+  if (!entity) {
+    entity = new Withdraw(withdrawID);
+  }
 
-export function handleUnpaused(event: Unpaused): void {}
-
-export function handleWithdraw(event: Withdraw): void {}
+  entity.sender = event.params.sender;
+  entity.token = event.params.token;
+  entity.amount = event.params.amount;
+  entity.liquidity = event.params.liquidity;
+  entity.to = event.params.to;
+  entity.timestamp = event.block.timestamp;
+  
+  entity.save();
+}
